6d31ef94f589c39c54d6e1c006254e7897ed5370
Bereket Worku <119013818+Bereket3@users.noreply.github.com>
2023-12-22 22:25:49 +0300

    Added solution for Design Bitset

---
+++ leetcode/design-bitset.py
@@ -0,0 +1,43 @@
+class Bitset:
+
+    def __init__(self, size: int):
+        self.bit = [False]*size
+        self.bitinv = [True]*size
+        self.ones = 0
+        self.zeros = size
+
+    def fix(self, idx: int) -> None:
+        if not self.bit[idx]:
+            self.zeros -= 1
+            self.ones += 1
+        self.bit[idx] = True
+        self.bitinv[idx] = False
+
+    def unfix(self, idx: int) -> None:
+        if self.bit[idx]:
+            self.zeros += 1
+            self.ones -= 1
+        self.bit[idx] = False
+        self.bitinv[idx] = True
+
+    def flip(self) -> None:
+        self.ones, self.zeros = self.zeros, self.ones
+        self.bit, self.bitinv = self.bitinv, self.bit
+
+    def all(self) -> bool:
+        return self.ones == self.bit.__len__()
+
+    def one(self) -> bool:
+        return self.ones > 0
+
+    def count(self) -> int:
+        return self.ones
+
+    def toString(self) -> str:
+        ans = ''
+        for bit in self.bit:
+            if bit:
+                ans += '1'
+            else:
+                ans += '0'
+        return ans
