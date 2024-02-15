afe542582753984240a5a95d6ad8499cde2a25bf
Bereket Worku <119013818+Bereket3@users.noreply.github.com>
2024-02-15 14:47:42 +0300

    Add solution for Rabbits in Forest

--- leetcode/leetcode/rabbits-in-forest.py
+++ leetcode/leetcode/rabbits-in-forest.py
@@ -1,14 +1,15 @@
 class Solution:
     def numRabbits(self, answers: List[int]) -> int:
-        count = 1
-        answers.sort()
-        tot = answers[0] + 1
-
-        for i in range(1, len(answers)):
-            if answers[i] != answers[i-1] or answers[i] - count + 1 == 0:
-                count = 1
-                tot += answers[i] + 1
-            else:
+        count = 0
+        mp = {}
+        for an in answers:
+            if an == 0:
                 count += 1
-
-        return tot
+            elif an not in mp or mp[an] == 0:
+                mp[an] = 1
+                count += an + 1
+            else:
+                mp[an] += 1
+                if mp[an] > an:
+                    mp[an] = 0
+        return count
