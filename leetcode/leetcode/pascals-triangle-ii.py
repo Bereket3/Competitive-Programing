770b7122a8561a294e1029e9ce4471abf0118e68
Bereket Worku <119013818+Bereket3@users.noreply.github.com>
2024-02-26 17:02:42 +0300

    Add solution for Pascal's Triangle II

---
+++ leetcode/leetcode/pascals-triangle-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def getRow(self, n: int) -> List[int]:
+        def triangle(n):
+            if n == 0:
+                return []
+            elif n == 1:
+                return [[1]]
+            else:
+                new_row = [1]
+                result = triangle(n-1)
+                last_row = result[-1]
+                for i in range(len(last_row)-1):
+                    new_row.append(last_row[i] + last_row[i+1])
+                new_row += [1]
+                result.append(new_row)
+            return result
+        return triangle(n + 1)[-1]
