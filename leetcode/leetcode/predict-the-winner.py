b7a62386b9b7ceac64c851d239cf0bad5dc45918
Bereket Worku <119013818+Bereket3@users.noreply.github.com>
2024-02-29 15:31:43 +0300

    Add solution for Predict the Winner

--- leetcode/leetcode/predict-the-winner.py
+++ leetcode/leetcode/predict-the-winner.py
@@ -1,20 +1,6 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-        has_c = [[False] * (len(nums) + 1) for i in range(len(nums) + 1)]
-        c = [[None] * (len(nums) + 1) for i in range(len(nums) + 1)]
-
-        def help(left, right):
-            if left > right:
-                return 0
-            
-            if has_c[left][right]:
-                return c[left][right]
-            
-            s_left = nums[left] - help(left + 1, right)
-            s_right = nums[right] - help(left, right - 1)
-
-            has_c[left][right] = True
-            c[left][right] = max(s_left, s_right)
-            return c[left][right]
-
-        return help(0, len(nums) -1) >= 0
+        #keep only the difference
+        def score(i,j):
+            return (i<=j) and max(nums[i]-score(i+1,j),nums[j]-score(i,j-1))
+        return score(0,len(nums)-1)>=0
